37cae56cd61cca1ea45671383777291e25d6e9c7
blazingkin <blazingkin@gmail.com>
2018-02-02 19:21:21 -0800

    Stop REPL from importing core packages the old way

--- com/blazingkin/interpreter/repl/REPL.java
+++ com/blazingkin/interpreter/repl/REPL.java
@@ -1,23 +1,13 @@
 package com.blazingkin.interpreter.repl;
 
-import java.io.File;
 import java.io.InputStream;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 import com.blazingkin.interpreter.Interpreter;
 import com.blazingkin.interpreter.executor.Executor;
-import com.blazingkin.interpreter.executor.instruction.Instruction;
-import com.blazingkin.interpreter.executor.sourcestructures.Method;
 import com.blazingkin.interpreter.expressionabstraction.ExpressionExecutor;
 import com.blazingkin.interpreter.variables.SystemEnv;
-import com.blazingkin.interpreter.variables.Value;
 import com.blazingkin.interpreter.variables.Variable;
-import com.blazingkin.interpreter.variables.VariableTypes;
-
-import in.blazingk.blz.packagemanager.ImportPackageInstruction;
-import in.blazingk.blz.packagemanager.Package;
 
 public class REPL {
 	
@@ -33,7 +23,6 @@ public class REPL {
 		String in = "";
 		Scanner sc = new Scanner(is);
 		Executor.immediateMode = true;
-		importCore();
 		Interpreter.thrownErrors.add(new Exception("There have been no exceptions"));
 		try{
 			do{
@@ -64,23 +53,6 @@ public class REPL {
 			}while (in.toLowerCase() != "exit");
 		}finally{
 			sc.close();
-		}
-	}
-	
-	private static void importCore(){
-		Set<File> packagesToImport = new HashSet<File>();
-		ImportPackageInstruction importer = (ImportPackageInstruction) Instruction.IMPORTPACKAGE.executor;
-		try{
-			packagesToImport.add(importer.findPackage("Core"));	
-			for (File f : packagesToImport){
-				Package p = new in.blazingk.blz.packagemanager.Package(f);
-				for (Method m : p.getAllMethodsInPackage()){
-					Variable.setValue(m.functionName, new Value(VariableTypes.Method, m));
-				}
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-			Interpreter.throwError(e.getMessage());
 		}
 	}
 
