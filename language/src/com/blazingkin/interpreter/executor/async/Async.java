54e5fb86d98b516f84d050f90f26946d662bb3f8
Alex Gravenor <blazingkin@gmail.com>
2018-07-14 15:31:03 -0700

    Better thread-local implementation of runtime stack. Make sure to set initial process in async

--- language/src/com/blazingkin/interpreter/executor/async/Async.java
+++ language/src/com/blazingkin/interpreter/executor/async/Async.java
@@ -1,18 +1,22 @@
 package com.blazingkin.interpreter.executor.async;
 
 import com.blazingkin.interpreter.BLZRuntimeException;
+import com.blazingkin.interpreter.executor.executionstack.RuntimeStack;
 import com.blazingkin.interpreter.executor.instruction.InstructionExecutorSemicolonDelimitedNode;
 import com.blazingkin.interpreter.expressionabstraction.ASTNode;
 import com.blazingkin.interpreter.variables.Context;
 import com.blazingkin.interpreter.variables.Value;
+import com.blazingkin.interpreter.executor.sourcestructures.Process;
 
 public class Async implements InstructionExecutorSemicolonDelimitedNode {
 
     public Value run(ASTNode[] nodes, Context context) {
         for (ASTNode node : nodes) {
+            Process currentProcess = RuntimeStack.getProcessStack().peek();
             new Thread(new Runnable() {
                 public void run(){
                     try {
+                        RuntimeStack.push(currentProcess);
                         node.execute(context);
                     }catch(BLZRuntimeException e) {
                         System.err.println("Don't have a solution for async errors yet: "+e.getMessage());
--- language/src/com/blazingkin/interpreter/executor/executionstack/RuntimeStack.java
+++ language/src/com/blazingkin/interpreter/executor/executionstack/RuntimeStack.java
@@ -5,53 +5,36 @@ import java.util.HashMap;
 
 import com.blazingkin.interpreter.BLZRuntimeException;
 import com.blazingkin.interpreter.executor.sourcestructures.Process;
-import com.blazingkin.interpreter.variables.Context;
-import com.blazingkin.interpreter.variables.Value;
 
 public class RuntimeStack {
 
-	public static HashMap<Long, ArrayDeque<Process>> threadProcessStack = new HashMap<Long, ArrayDeque<Process>>();
+	public static ThreadLocal<ArrayDeque<Process>> threadProcessStack = new ThreadLocal<ArrayDeque<Process>>() {
+		@Override
+		protected ArrayDeque<Process> initialValue() {
+			return new ArrayDeque<Process>();
+		}
+	};
 
 
 	
 	public static void push(Process process) throws BLZRuntimeException{
-		ArrayDeque<Process> pStack = threadProcessStack.get(Thread.currentThread().getId());
-		if (pStack == null) {
-			threadProcessStack.put(Thread.currentThread().getId(), new ArrayDeque<Process>());
-			pStack = threadProcessStack.get(Thread.currentThread().getId());
-		}
-		pStack.push(process);
+		threadProcessStack.get().push(process);
 		process.onBlockStart();
 	}
 	
 	public static void pop(){
-		ArrayDeque<Process> pStack = threadProcessStack.get(Thread.currentThread().getId());
-		if (pStack == null) {
-			threadProcessStack.put(Thread.currentThread().getId(), new ArrayDeque<Process>());
-			return;
-		}
-		pStack.pop();
+		threadProcessStack.get().pop();
 	}
 
 	public static boolean isEmpty() {
-		ArrayDeque<Process> pStack = threadProcessStack.get(Thread.currentThread().getId());
-		if (pStack == null) {
-			threadProcessStack.put(Thread.currentThread().getId(), new ArrayDeque<Process>());
-			return true;
-		}
-		return pStack.isEmpty();
+		return threadProcessStack.get().isEmpty();
 	}
 
 	public static ArrayDeque<Process> getProcessStack() {
-		ArrayDeque<Process> pStack = threadProcessStack.get(Thread.currentThread().getId());
-		if (pStack == null) {
-			threadProcessStack.put(Thread.currentThread().getId(), new ArrayDeque<Process>());
-			pStack = threadProcessStack.get(Thread.currentThread().getId());
-		}
-		return pStack;
+		return threadProcessStack.get();
 	}
 	
 	public static void cleanup(){
-		threadProcessStack.clear();
+		threadProcessStack.get().clear();
 	}
 }
