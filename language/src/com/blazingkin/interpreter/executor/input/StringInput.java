d19689ede61427ef8be830c0009c88c5df5bb42b
blazingkin <blazingkin@gmail.com>
2018-11-10 17:41:18 -0800

    Fix bug where string_input didn't return anythign

--- language/src/com/blazingkin/interpreter/executor/input/StringInput.java
+++ language/src/com/blazingkin/interpreter/executor/input/StringInput.java
@@ -1,16 +1,18 @@
 package com.blazingkin.interpreter.executor.input;
 
 import com.blazingkin.interpreter.executor.Executor;
-import com.blazingkin.interpreter.executor.instruction.InstructionExecutorStringArray;
+import com.blazingkin.interpreter.executor.instruction.InstructionExecutor;
 import com.blazingkin.interpreter.variables.Value;
+import com.blazingkin.interpreter.variables.Context;
 
-public class StringInput implements InstructionExecutorStringArray {
+public class StringInput implements InstructionExecutor {
 	/*	StringInput
 	 * 	Gets a string as input
 	 * 
 	 */
-	public Value run(String[] vars){
-		return Value.string(Executor.getEventHandler().getInput());
+	public Value run(String l, Context c){
+		String result = Executor.getEventHandler().getInput();
+		return Value.string(result);
 	}
 
 	
